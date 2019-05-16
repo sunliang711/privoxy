b1e04246253bbd8e6fc9bf49f0739062857984a9
sunliang <sunliang711@163.com>
2019-05-16 14:05:58 +0800

    check name.sh return value

--- queryById.sh
+++ queryById.sh
@@ -4,11 +4,13 @@ db="$(cat db)"
 # the following line comes from install.sh
 # sqlite3 "$db" "CREATE TABLE config(id integer primary key autoincrement,name varchar,local_port int unique,upstream varchar,logdir varchar,ispac int);"
 sqliteCommand="select name,local_port,upstream,logdir,ispac from config where id=$id;"
-echo "sqliteCommand: $sqliteCommand" 1>&2
+# for DEBUG
+# echo "sqliteCommand: $sqliteCommand" 1>&2
 record=$(sqlite3 $db "$sqliteCommand")
 if [ -z "$record" ];then
     echo "No config record with id: $id" 1>&2
     exit 1
 fi
-echo "record: $record" 1>&2
+# for DEBUG
+# echo "record: $record" 1>&2
 echo "$record"
